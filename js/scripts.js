2e61c037bc7feedac9b393f93e584aac15952e4d
Matt Foley <mattfoley0375@gmail.com>
2021-01-29 09:52:45 -0800

    add ui

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
 //Business Logic Top
-function pizza(orderForName) {
+function pizza() {
   this.size
   this.sauce
   this.topping
 }
+
+pizza.prototype.pieSize = function(size) {
+
+}
+
+
+//User Logic Bottom
+
+// User Interface Logic //
+
+$(document).ready(function() {
+  $("form#orderForm").submit(function(event) {
+    event.preventDefault();
+
+
+    let pieSize = $("input:radio[name=size]:checked").val();
